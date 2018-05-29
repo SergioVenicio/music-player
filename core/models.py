5fe9f3a76831fe8facd10e6bdd848cd51fd97ef8
sergio <sergionirvana2010@hotmail.com>
2018-05-28 21:37:23 -0300

    Alteração da pasta de imagens

--- core/models.py
+++ core/models.py
@@ -80,7 +80,7 @@ class Album(models.Model):
     nome = models.CharField(max_length=250)
     banda = models.ForeignKey(Banda, on_delete=models.CASCADE)
     data_lancamento = models.PositiveIntegerField()
-    capa = models.ImageField(('Capa'), upload_to='static/capas/', blank=True)
+    capa = models.ImageField(('Capa'), upload_to='capas', blank=True)
 
     def __str__(self):
         return self.nome
--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
-from . import forms
-from .utils import get_all_musics, get_albuns
+"""
+    Views básicas do projeto, music-player
+"""
+
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from .utils import get_all_musics, get_albuns
+from . import forms
 
 
 def home(request):
+    """ Retorna todos os albuns cadastrados no sistema """
     context = {
         'albuns': get_albuns()
     }
@@ -11,6 +17,7 @@ def home(request):
 
 
 def musicas(request, album_id):
+    """ Retorna as músicas cadastradas no sistema com base no album """
     context = {
         'musics': get_all_musics(album_id=album_id),
         'album': get_albuns(album_id=album_id)
@@ -19,9 +26,12 @@ def musicas(request, album_id):
 
 
 def add_musicas(request):
+    """ Adiciona musicas no banco de dados """
     if request.method == 'GET':
         form = forms.MusicaForm()
         context = {
             'form': form
         }
         return render(request, 'core/add_music.html', context=context)
+    else:
+        raise PermissionDenied()

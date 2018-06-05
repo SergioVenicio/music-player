cfab61a166afb3b8d6fcd86de937da0338f384f9
sergio <sergionirvana2010@hotmail.com>
2018-06-05 10:03:19 -0300

    Senha default python decouple

--- music_player/settings.py
+++ music_player/settings.py
@@ -3,7 +3,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='senha_secreta')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 

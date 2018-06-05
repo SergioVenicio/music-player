73b49062bde8dd4bae3177f73baff05b2528d4d2
sergio <sergionirvana2010@hotmail.com>
2018-06-04 23:26:42 -0300

    Configuração de arquivos estaticos

--- music_player/settings.py
+++ music_player/settings.py
@@ -87,8 +87,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+     os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

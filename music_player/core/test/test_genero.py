72737290de2c6770d2a1683bb74f3566e4847577
sergio <sergionirvana2010@hotmail.com>
2018-06-08 11:48:34 -0300

    Criação de capas no teste de criação de generos

--- music_player/core/test/test_genero.py
+++ music_player/core/test/test_genero.py
@@ -1,9 +1,18 @@
 import pytest
+import base64
 from music_player.core.models import Genero
+from django.core.files.base import ContentFile
+from music_player.core.utils import decode_file
 
 
 @pytest.mark.django_db(transaction=True)
 def test_criacao_genero():
-    genero = Genero.objects.create(descricao='Teste')
+    path = 'music_player/core/test'
+    capa = open(path + '/imagem_test.png', 'rb').read()
+    b64_capa = base64.b64encode(capa)
+    capa = ContentFile(decode_file(b64_capa), 'teste.png')
+    genero = Genero(descricao='Teste', imagen=capa)
+    genero.save()
     assert isinstance(genero.id, int)
     assert 'Teste' == genero.descricao
+    assert genero.imagen is not None

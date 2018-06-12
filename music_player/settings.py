a5c14bcd541361359bb5e5ddb5707bdc2481cc55
sergio <sergionirvana2010@hotmail.com>
2018-06-12 11:03:46 -0300

    Revert "Documentação dos modelos"
    This reverts commit ac8fcbbd470c1b16524bdacaf4dbb9a09d56a36f.

--- music_player/settings.py
+++ music_player/settings.py
@@ -3,6 +3,7 @@ from decouple import config
 from django.utils.crypto import get_random_string
 
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = get_random_string(50)

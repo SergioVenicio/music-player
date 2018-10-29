ffe5f433232adc9fe3afae4d2f825ba589328051
Sérgio Andrade <sergio.andrade@sensedata.com.br>
2018-10-29 15:58:42 -0300

    Change django restframework router register base_name param to basename django_restframework (3.9.0)

--- music_player/api/urls.py
+++ music_player/api/urls.py
@@ -4,23 +4,23 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'genero', viewset.GeneroViewSet, base_name='genero')
-router.register(r'banda', viewset.BandaViewSet, base_name='banda')
-router.register(r'album', viewset.AlbumViewSet, base_name='album')
-router.register(r'musicas', viewset.MusicaViewSet, base_name='musica')
+router.register(r'genero', viewset.GeneroViewSet, basename='genero')
+router.register(r'banda', viewset.BandaViewSet, basename='banda')
+router.register(r'album', viewset.AlbumViewSet, basename='album')
+router.register(r'musicas', viewset.MusicaViewSet, basename='musica')
 router.register(
     r'musicas/album/(?P<album_id>.+)', viewset.MusicaViewSet,
-    base_name='musica_filter'
+    basename='musica_filter'
 )
-router.register(r'usuarios', viewset.UsuarioViewSet, base_name='usuario')
-router.register(r'likes', viewset.LikesViewSet, base_name='like')
+router.register(r'usuarios', viewset.UsuarioViewSet, basename='usuario')
+router.register(r'likes', viewset.LikesViewSet, basename='like')
 router.register(
     r'likes/usuario/(?P<usuario_id>\w+)/(?P<musica_id>\w+)',
-    viewset.LikesViewSet, base_name='like_filter_music'
+    viewset.LikesViewSet, basename='like_filter_music'
 )
 router.register(
     r'likes/usuario/(?P<usuario_id>.+)',
-    viewset.LikesViewSet, base_name='like_filter'
+    viewset.LikesViewSet, basename='like_filter'
 )
 
 urlpatterns = [

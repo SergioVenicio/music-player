efb20c95f506b86846637d01cfacb5fe2c8eb7ba
sergio <sergiovenicio2015@gmail.com>
2020-11-22 10:08:47 -0300

    Remove pages from backend

--- music_player/core/admin.py
+++
@@ -1,9 +0,0 @@
-from music_player.core import models, forms
-from django.contrib import admin
-
-
-admin.site.register(models.Genero)
-admin.site.register(models.Banda)
-admin.site.register(models.Album)
-admin.site.register(models.Musica)
-admin.site.register(models.Usuario, forms.UsuarioAdmin)
--- music_player/urls.py
+++ music_player/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
@@ -19,8 +18,6 @@ urlpatterns = [
     path('api/v1/token/refresh', TokenRefreshView.as_view()),
     path('api/v1/token/verify', TokenVerifyView.as_view()),
     path('api/v1/', include(api_router.urls)),
-    path('', include('music_player.core.urls')),
-    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
